14983fb496cfe3e5df4d99b34b018e95f1564860
JuanEs-az <juarangoz@unal.edu.co>
2024-03-10 23:54:23 -0500

    First commit: Ejercicio00 de prueba

---
+++ src/ejercicios/Ejercicio00.java
@@ -0,0 +1,7 @@
+package ejercicios;
+
+public class Ejercicio00 {
+    public static void main(String[] args) {
+        System.out.println("Ésto es un ejemplo de un archivo de ejercicio.");
+    }
+}
