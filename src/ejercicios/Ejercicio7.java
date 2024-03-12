c2415bdba9fcbb0f79489d10b52ca018e8630bc4
Mariluz Caro <159815558+mcaror17@users.noreply.github.com>
2024-03-11 23:59:23 -0500

    Capitulo 4 Ejercicio 7

---
+++ src/ejercicios/Ejercicio7.java
@@ -0,0 +1,26 @@
+package actividad.pkg2;
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        double a, b;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numero A: ");
+        a = entrada.nextDouble(); 
+        System.out.println("Ingrese un numero B: ");
+        b = entrada.nextDouble(); 
+        if (a > b) {
+            System.out.println(a + " es mayor que " + b);           
+        }
+        else if (a == b){
+            System.out.println(a + " es igual a " + b);
+        }
+        else{
+            System.out.println(a + " es menor que  " + b);
+        }
+    
+    }
+
+}
+
