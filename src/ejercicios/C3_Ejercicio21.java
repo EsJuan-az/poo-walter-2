2449c3f1152875dcf02c5a8c69e1a9d7ab3f4156
Mariluz Caro <159815558+mcaror17@users.noreply.github.com>
2024-03-12 11:31:29 -0500

    C3_Ejercicio21.java

--- src/ejercicios/C3_Ejercicio21.java
+++ src/ejercicios/C3_Ejercicio21.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class C3_Ejercicio21 {
     
     public static void main(String[] args) {
-        double lado1, lado2, base, perim, altura, area;
+        double lado1, lado2, base, perim, semiper, altura, area;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Base del triangulo: ");
         base = entrada.nextDouble();
@@ -13,10 +13,11 @@ public class C3_Ejercicio21 {
         System.out.println("Lado 2 del triangulo: ");
         lado2 = entrada.nextDouble();
         perim = lado1+lado2+base;
+        semiper = perimetro/2;
         altura = Math.sqrt(3) * base / 2;
         area = (base * altura ) / 2;
         System.out.println("Perimetro del triangulo: " + perim);
-        System.out.println("Altura del triangulo: " + altura);
+        System.out.println("Semiperimetro del triangulo: " + semiper);
         System.out.println("Area del triangulo: " + area);
           
     }
